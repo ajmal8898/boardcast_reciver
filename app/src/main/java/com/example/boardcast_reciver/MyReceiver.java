b7b5a867b4e03df1317426682db6267e35c748ab
ajmal8898 <ajmal.m@inxsasia.com>
2021-04-01 12:16:18 +0530

    boardcast

---
+++ app/src/main/java/com/example/boardcast_reciver/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.example.boardcast_reciver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String status = NetworkUtil.getConnectivityStatusString(context);
+        if (status.isEmpty()) {
+            status = "No Internet Connection";
+        }
+        Toast.makeText(context, status, Toast.LENGTH_LONG).show();
+    }
+}
+
